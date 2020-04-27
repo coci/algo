7dd5da83fad8556da1a3669a654dca1e69e8fa9a
soroush <soroush_safarii@yahoo.com>
2020-04-27 14:06:45 +0430

    fix same digits

--- example/reverse_int.py
+++ example/reverse_int.py
@@ -18,14 +18,15 @@ def reverse_int_without_string(number):
 	while number > 0 :
 		digits_number.append(number%10)
 		number = number // 10
-
+	print(digits_number)
 	# multiple number with her index with 10 = > 123 => 1*100 + 2* 10 + 3 * 1
 	number = [] 
 	for i in digits_number :
-		for j in range(len(digits_number[digits_number.index(i):])-1):
+		digits_number = digits_number[1:]
+		for j in range(len(digits_number)):
+			print(j)
 			i *= 10
 		number.append(i)
-
 	return -(sum(number))
 
 
